fc356622ec411adf6413b70e803813975a9ffb8e
Eduardo Rodrigues da Silva <silva.rodriguesedu@icloud.com>
2021-01-25 23:04:56 -0300

    Steps 10, 11 - ProdutoController, ExceptionResponse, CrudExceptionHandler created

--- crud/src/main/java/com/eduardo/crud/data/vo/ProdutoVO.java
+++ crud/src/main/java/com/eduardo/crud/data/vo/ProdutoVO.java
@@ -3,24 +3,41 @@ package com.eduardo.crud.data.vo;
 import java.io.Serializable;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.RepresentationModel;
 
 import com.eduardo.crud.entity.Produto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @JsonPropertyOrder({"id","nome","estoque","preco"})
-@Data
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode//(callSuper = false)
-public class ProdutoVO implements Serializable {
+@EqualsAndHashCode(callSuper = false)
+public class ProdutoVO extends RepresentationModel<ProdutoVO> implements Serializable {
 
 	private static final long serialVersionUID = 2938272643682548375L;
+	
+	public ProdutoVO() {
+		
+	}
+
+	public ProdutoVO(Long id, String nome, Integer estoque, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.estoque = estoque;
+		this.preco = preco;
+	}
 
 	@JsonProperty("id")
 	private Long id;
@@ -45,6 +62,36 @@ public class ProdutoVO implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public String toString() {
+		return "ProdutoVO [id=" + id + ", nome=" + nome + ", estoque=" + estoque + ", preco=" + preco + "]";
+	}
+	
 	
 	
 }
